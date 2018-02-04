bfd79282af1cf96be67fb581e5ec65e8badab1d7
Jesús Eduardo <heckyel@hyperbola.info>
2018-02-04 17:34:56 -0500

    pep8 on banner.py

--- udemy/colorized/banner.py
+++ udemy/colorized/banner.py
@@ -1,16 +1,17 @@
 from .Output import *
 
+
 def banner():
     banner = '''
-%s%s              __                               ____  
-%s%s   __  ______/ /__  ____ ___  __  __      ____/ / /  
-%s%s  / / / / __  / _ \/ __ `__ \/ / / /_____/ __  / /   
-%s%s / /_/ / /_/ /  __/ / / / / / /_/ /_____/ /_/ / /    
-%s%s \__,_/\__,_/\___/_/ /_/ /_/\__, /      \__,_/_/     
+%s%s              __                               ____
+%s%s   __  ______/ /__  ____ ___  __  __      ____/ / /
+%s%s  / / / / __  / _ \/ __ `__ \/ / / /_____/ __  / /
+%s%s / /_/ / /_/ /  __/ / / / / / /_/ /_____/ /_/ / /
+%s%s \__,_/\__,_/\___/_/ /_/ /_/\__, /      \__,_/_/
 %s%s                           /____/
                                  %s%sVersion : %s%s0.3
                                  %s%sAuthor  : %s%sNasir khan(r0ot h3x49)
                                  %s%sVisit   : %s%shttps://github.com/r0oth3x49
 
-''' % (fc, sb,fc, sb,fc, sb, fm, sb, fy, sb, fy, sb, fy,sd, fg, sd, fy, sd, fg, sd, fy, sd, fg, sd)
+''' % (fc, sb, fc, sb, fc, sb, fm, sb, fy, sb, fy, sb, fy, sd, fg, sd, fy, sd, fg, sd, fy, sd, fg, sd)
     return banner
