d388606553de576f68b9ecb70cfaceefd551002b
Jesús Eduardo <heckyel@hyperbola.info>
2018-02-04 16:44:14 -0500

    pep8 on __init__.py

--- udemy/__init__.py
+++ udemy/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-__version__ = "0.3"
-__author__  = "Nasir Khan(r0ot h3x49)"
+from ._downloader import Downloader
+from ._extractor import UdemyInfoExtractor
+from ._getpass import GetPass
+from ._utils import (
+    cache_credentials,
+    use_cached_credentials,
+    WEBVTT2SRT
+    )
 
-from 	._downloader 	import Downloader
-from 	._extractor 	import UdemyInfoExtractor
-from 	._getpass 		import GetPass
-from 	._utils 		import (
-								cache_credentials,
-								use_cached_credentials,
-								WEBVTT2SRT
-							)
+__version__ = "0.3"
+__author__ = "Nasir Khan(r0ot h3x49)"
