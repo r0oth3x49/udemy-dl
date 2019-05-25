c56a725022daffb837d84bc5ce3b232eca477c62
r0oth3x49 <r0oth3x49@gmail.com>
2019-05-25 13:30:56 +0500

    updated code quality for login, thanks to  01ttouch for the quick fix..

--- udemy/_compat.py
+++ udemy/_compat.py
@@ -75,7 +75,7 @@ MY_COURSES_URL = "https://{portal_name}.udemy.com/api-2.0/users/me/subscribed-co
 COURSE_SEARCH = "https://{portal_name}.udemy.com/api-2.0/users/me/subscribed-courses?fields[course]=id,url,published_title&page=1&page_size=1000&ordering=-access_time&search={course_name}"
 COURSE_URL = 'https://{portal_name}.udemy.com/api-2.0/courses/{course_id}/cached-subscriber-curriculum-items?fields[asset]=results,external_url,time_estimation,download_urls,slide_urls,filename,asset_type,captions,stream_urls,body&fields[chapter]=object_index,title,sort_order&fields[lecture]=id,title,object_index,asset,supplementary_assets,view_html&page_size=10000'
 HEADERS = {
-            'Host': 'www.udemy.com',
+            'Origin': 'www.udemy.com',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0',
             'Referer': 'https://www.udemy.com/join/login-popup/',
             'Accept': '*/*',
