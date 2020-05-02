2e429eccdb3dd78b3b8c3c64b69ec93ca3f36aae
r0oth3x49 <r0oth3x49@gmail.com>
2020-05-02 12:47:03 +0500

    fixed #490.

--- udemy/_colorized/colors.py
+++ udemy/_colorized/colors.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-'''
+"""
 
 Author  : Nasir Khan (r0ot h3x49)
 Github  : https://github.com/r0oth3x49
@@ -20,60 +20,31 @@ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVE
 ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
 THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-'''
+"""
 
-from colorama import init,Fore,Back,Style
-import os
+from colorama import init, Fore, Back, Style
 
-if os.name == "posix":
-    ## ----------------------------------------------------------------------------------------------------------------------  ##
-    init(autoreset=True)
-    # colors foreground text:
-    fc = "\033[0;96m"
-    fg = "\033[0;92m"
-    fw = "\033[0;97m"
-    fr = "\033[0;91m"
-    fb = "\033[0;94m"
-    fy = "\033[0;33m"
-    fm = "\033[0;35m"
+init(autoreset=True)
+# colors foreground text:
+fc = Fore.CYAN
+fg = Fore.GREEN
+fw = Fore.WHITE
+fr = Fore.RED
+fb = Fore.BLUE
+fy = Fore.YELLOW
+fm = Fore.MAGENTA
 
-    # colors background text:
-    bc = "\033[46m"
-    bg = "\033[42m"
-    bw = "\033[47m"
-    br = "\033[41m"
-    bb = "\033[44m"
-    by = "\033[43m"
-    bm = "\033[45m"
 
-    # colors style text:
-    sd = Style.DIM
-    sn = Style.NORMAL
-    sb = Style.BRIGHT
-else:
-    ## ----------------------------------------------------------------------------------------------------------------------  ##
-    init(autoreset=True)
-    # colors foreground text:
-    fc = Fore.CYAN
-    fg = Fore.GREEN
-    fw = Fore.WHITE
-    fr = Fore.RED
-    fb = Fore.BLUE
-    fy = Fore.YELLOW
-    fm = Fore.MAGENTA
-    
+# colors background text:
+bc = Back.CYAN
+bg = Back.GREEN
+bw = Back.WHITE
+br = Back.RED
+bb = Back.BLUE
+by = Fore.YELLOW
+bm = Fore.MAGENTA
 
-    # colors background text:
-    bc = Back.CYAN
-    bg = Back.GREEN
-    bw = Back.WHITE
-    br = Back.RED
-    bb = Back.BLUE
-    by = Fore.YELLOW
-    bm = Fore.MAGENTA
-
-    # colors style text:
-    sd = Style.DIM
-    sn = Style.NORMAL
-    sb = Style.BRIGHT
-    ## ----------------------------------------------------------------------------------------------------------------------  ##
+# colors style text:
+sd = Style.DIM
+sn = Style.NORMAL
+sb = Style.BRIGHT
